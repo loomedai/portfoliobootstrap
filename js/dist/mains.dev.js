22a896942c1547652db77c2f523bee0c3e71a155
loomedai <bbrith@hotmail.com>
2023-02-13 15:20:31 +0100

    portfolio scroll

--- js/dist/mains.dev.js
+++ js/dist/mains.dev.js
@@ -13,4 +13,29 @@ gsap.from('.world', {
   duration: 1.5,
   y: -20,
   ease: "expo.inOut"
+}); //portfolio//
+
+gsap.registerPlugin(ScrollTrigger);
+var lenis = new Lenis({
+  duration: 0.5,
+  easing: function easing(t) {
+    return Math.min(1, 1.001 - Math.pow(2, -5 * t));
+  }
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.img',
+    scrub: true
+  }
+}).to('.img', {
+  stagger: .2,
+  y: -700,
+  scrub: true
 });
--- js/mains.js
+++ js/mains.js
@@ -14,5 +14,33 @@ gsap.from('.world',{
     ease:"expo.inOut"
 })
 
+//portfolio//
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+const lenis = new Lenis({
+  duration: 0.5,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -5 * t))
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.img',
+    scrub: true
+  }
+})
+.to('.img', {
+  stagger: .2,
+  y: -700,
+  scrub: true
+})
 
 
